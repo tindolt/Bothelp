d77c0176f7d14956953801112bbe05a26f049bad
Clifton Burns <54863218+CaptnLucid@users.noreply.github.com>
2023-11-19 05:09:51 -0500

    Create interactionCreate.js

---
+++ interactionCreate.js
@@ -0,0 +1,62 @@
+const { Events } = require("discord.js");
+
+// Kleur is a terminal colorizer similar to Chalk
+const { red, bold, underline } = require("kleur");
+
+// Define a new Map to hold the cooldowns
+const cooldowns = new Map();
+
+module.exports = {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+      console.error(
+        "No command matching " +
+          red().bold().underline(`${interaction.commandName}`) +
+          " was found."
+      );
+      return;
+    }
+
+    // Cooldown logic
+    if (!cooldowns.has(command.name)) {
+      cooldowns.set(command.name, new Map());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000; // Cooldown period in milliseconds
+
+    if (timestamps.has(interaction.user.id)) {
+      const expirationTime =
+        timestamps.get(interaction.user.id) + cooldownAmount;
+
+      if (now < expirationTime) {
+        const timeLeft = (expirationTime - now) / 1000;
+        return interaction.reply(
+          `Please wait ${timeLeft.toFixed(
+            1
+          )} more second(s) before reusing the \`${command.name}\` command.`
+        );
+      }
+    }
+
+    timestamps.set(interaction.user.id, now);
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(
+        "Error executing " +
+          red().bold().underline(`${interaction.commandName}`) +
+          "."
+      );
+      console.error(error);
+    }
+  },
+};
