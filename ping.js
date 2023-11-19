f20d3ae244fd9697ee48ecfcfa3a60ad2d01ae6b
Clifton Burns <54863218+CaptnLucid@users.noreply.github.com>
2023-11-19 05:11:29 -0500

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  category: "utility",
+  // Can comment out cooldown line to put default to 3 seconds instead of 10.
+  cooldown: 10
+  async execute(interaction) {
+    return interaction.reply("Pong!");
+  },
+};
